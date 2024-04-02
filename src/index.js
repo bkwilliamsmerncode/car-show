3cb3d025d2796fc1b1434de87b0d09b63d8f9049
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-04-01 21:30:34 -0700

    1st commit
    commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+import "./Global.css"
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />)
+
+
